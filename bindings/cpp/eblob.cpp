e017827ae4e07aef9208681f29050385ee4e9b0e
Evgeniy Polyakov <zbr@ioremap.net>
2011-06-01 05:29:54 +0400

    Added cpp bindings

---
+++ bindings/cpp/eblob.cpp
@@ -0,0 +1,46 @@
+/*
+ * 2011+ Copyright (c) Evgeniy Polyakov <zbr@ioremap.net>
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <sys/types.h>
+
+#include <unistd.h>
+#include <string.h>
+
+#include <eblob/cppdef.hpp>
+
+eblob::eblob(const char *log_file, const unsigned int log_mask, const std::string &eblob_path) :
+	logger_(log_file, log_mask)
+{
+	std::stringstream mstr;
+
+	mstr << eblob_path << ".mmap." << getpid();
+
+	struct eblob_config cfg;
+
+	memset(&cfg, 0, sizeof(cfg));
+	cfg.file = (char *)eblob_path.c_str();
+	cfg.mmap_file = (char *)mstr.str().c_str();
+	cfg.log = logger_.log();
+
+	eblob_ = eblob_init(&cfg);
+	if (!eblob_) {
+		throw std::runtime_error("Failed to initialize eblob");
+	}
+}
+
+eblob::~eblob()
+{
+	eblob_cleanup(eblob_);
+}
---
+++ include/eblob/cppdef.hpp
@@ -0,0 +1,71 @@
+/*
+ * 2011+ Copyright (c) Evgeniy Polyakov <zbr@ioremap.net>
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __EBLOB_CPPDEF_H
+#define __EBLOB_CPPDEF_H
+
+#include <stdio.h>
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <stdexcept>
+
+#include <eblob/blob.h>
+
+class eblob_logger {
+	public:
+		eblob_logger(const char *log_file, const unsigned int log_mask) : log_file_(log_file) {
+			file_ = fopen(log_file, "a");
+			if (!file_) {
+				std::ostringstream str;
+				str << "Failed to open log file " << log_file_;
+				throw std::runtime_error(str.str());
+			}
+
+			logger_.log_private = file_;
+			logger_.log_mask = log_mask;
+			logger_.log = eblob_log_raw_formatted;
+		}
+
+		eblob_logger(const eblob_logger &l) {
+			eblob_logger(l.log_file_.c_str(), l.logger_.log_mask);
+		}
+
+		virtual ~eblob_logger() {
+			fclose(file_);
+		}
+
+		struct eblob_log *log() {
+			return &logger_;
+		}
+	private:
+		struct eblob_log	logger_;
+		FILE			*file_;
+		std::string		log_file_;
+};
+
+class eblob {
+	public:
+		eblob(const char *log_file, const unsigned int log_mask, const std::string &eblob_path);
+		virtual ~eblob();
+
+	private:
+		eblob_logger		logger_;
+		struct eblob_backend	*eblob_;
+};
+
+#endif /* __EBLOB_CPPDEF_H */
+
