96fe5afbd0eb83b26a1e7a32bd53995139487ce9
Evgeniy Polyakov <zbr@ioremap.net>
2011-06-01 08:39:31 +0400

    Use munmap/mmap instead of mremap, sine the former may fail miserably

--- library/hash.c
+++ library/hash.c
@@ -231,11 +231,13 @@ static int eblob_realloc_entry_array(struct eblob_hash *hash, struct eblob_hash_
 			hash->file_size = new_size;
 		}
 
-		new_base = mremap(hash->map_base, hash->map_size, new_size, 0);
+		new_base = mmap(hash->map_base, hash->file_size, PROT_WRITE | PROT_READ, MAP_SHARED, hash->map_fd, 0);
 		if (new_base == MAP_FAILED) {
 			err = -ENOMEM;
 			goto err_out_unlock;
 		}
+
+		munmap(hash->map_base, hash->map_size);
 
 		hash->map_size = new_size;
 		hash->map_base = new_base;
