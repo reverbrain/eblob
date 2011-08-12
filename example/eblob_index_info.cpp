c20b5c8d72320b745821f1e25055de81a4d6c32a
Evgeniy Polyakov <zbr@ioremap.net>
2011-08-12 23:48:04 +0400

    Added id search into eblob_index_info structure

--- example/eblob_index_info.cpp
+++ example/eblob_index_info.cpp
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <string.h>
 
 #include <fstream>
 #include <iostream>
@@ -6,22 +8,64 @@
 
 #include <eblob/blob.h>
 
-int main(int argc, char *argv[]) {
+static int dnet_parse_numeric_id(char *value, unsigned char *id)
+{
+	unsigned char ch[5];
+	unsigned int i, len = strlen(value);
 
-	if (argc != 2) {
-		std::cerr << "Usage: " << argv[0] << " eblob.index " << std::endl;
-		return(-1);
+	memset(id, 0, EBLOB_ID_SIZE);
+
+	if (len/2 > EBLOB_ID_SIZE)
+		len = EBLOB_ID_SIZE * 2;
+
+	ch[0] = '0';
+	ch[1] = 'x';
+	ch[4] = '\0';
+	for (i=0; i<len / 2; i++) {
+		ch[2] = value[2*i + 0];
+		ch[3] = value[2*i + 1];
+
+		id[i] = (unsigned char)strtol((const char *)ch, NULL, 16);
 	}
 
-	std::ifstream ind(argv[1], std::ios_base::in | std::ios_base::binary);
+	if (len & 1) {
+		ch[2] = value[2*i + 0];
+		ch[3] = '0';
+
+		id[i] = (unsigned char)strtol((const char *)ch, NULL, 16);
+	}
+
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	struct eblob_key key;
+	int check_key_len = 0;
+
+	if (argc < 2) {
+		std::cerr << "Usage: " << argv[0] << " eblob.index <id>" << std::endl;
+		return(-1);
+	} else if (argc > 2) {
+		memset(&key, 0, sizeof(struct eblob_key));
+		dnet_parse_numeric_id(argv[2], key.id);
+		check_key_len = strlen(argv[2]) / 2;
+	}
+
 	struct eblob_disk_control dc;
-	int total = 0, removed = 0;
+	long long total = 0, removed = 0;
+	char id_str[EBLOB_ID_SIZE * 2 + 1]; 
 
 	try {
+		std::ifstream ind(argv[1], std::ios_base::in | std::ios_base::binary);
+
 		while (!ind.eof()) {
 			ind.read((char *)&dc, sizeof(dc));
 
-			if (ind.gcount() == 0) break;
+			eblob_convert_disk_control(&dc);
+
+			if (ind.gcount() == 0)
+				break;
 
 			if (ind.gcount() != sizeof(dc))
 				throw std::runtime_error("Index read failed");
@@ -29,6 +73,29 @@ int main(int argc, char *argv[]) {
 			total++;
 			if (dc.flags & BLOB_DISK_CTL_REMOVE)
 				removed++;
+
+			if (check_key_len) {
+				if (memcmp(dc.key.id, key.id, check_key_len) == 0) {
+					std::cout << eblob_dump_id_len_raw(dc.key.id, EBLOB_ID_SIZE, id_str) << ": " <<
+						"data_size: " << dc.data_size << ", " <<
+						"disk_size: " << dc.disk_size << ", " <<
+						"position: " << dc.position << ", " <<
+						"flags: " << std::hex << dc.flags << std::dec;
+
+					std::string flags = " [ ";
+					if (dc.flags &  BLOB_DISK_CTL_NOCSUM)
+						flags += "NO_CSUM ";
+					if (dc.flags &  BLOB_DISK_CTL_COMPRESS)
+						flags += "COMPRESS ";
+					if (dc.flags &  BLOB_DISK_CTL_REMOVE)
+						flags += "REMOVED ";
+
+					if (flags.size() > 3) {
+						std::cout << flags << "]";
+					}
+					std::cout << std::endl;
+				}
+			}
 		}
 	} catch (const std::exception &e) {
 		std::cerr << e.what() << std::endl;
