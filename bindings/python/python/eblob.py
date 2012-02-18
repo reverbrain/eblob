aef86bf30f3cdf7caa1b426a9084b2dde24b64b1
Evgeniy Polyakov <zbr@ioremap.net>
2012-02-19 00:05:42 +0400

    Added more stuff into simple binding

--- bindings/python/python/eblob.py
+++ bindings/python/python/eblob.py
@@ -6,11 +6,12 @@ class blob:
 
 	FLAGS_REMOVED = 1
 
-	def __init__(self, path, mode='r+b'):
-		self.dataf = open(path, mode)
-		self.index = open(path + '.index', mode)
+	def __init__(self, path, data_mode='r+b', index_mode='r+b'):
+		self.dataf = open(path, data_mode)
+		self.index = open(path + '.index', index_mode)
 
 		self.position = 0
+		self.next_position = 0
 		self.id = ''
 		self.data_size = 0
 		self.disk_size = 0
@@ -23,14 +24,16 @@ class blob:
 			raise NameError('Finished index')
 
 		self.id, self.flags, self.data_size, self.disk_size, self.position = struct.unpack(self.format, idata)
-		self.eid = elliptics_id(list(bytearray(self.id)), 0, 0)
+		self.read_data_index()
 
-		self.dataf.seek(self.position)
+	def read_data_index(self):
+		self.dataf.seek(self.next_position)
 		ddata = self.dataf.read(self.index_size)
 		if len(ddata) != self.index_size:
 			raise NameError('Finished data')
 
 		self.id, self.flags, self.data_size, self.disk_size, self.position = struct.unpack(self.format, ddata)
+		self.next_position = self.position + self.disk_size
 	
 	def removed(self):
 		return self.flags & self.FLAGS_REMOVED
@@ -52,17 +55,32 @@ class blob:
 
 		self.dataf.seek(self.position)
 		self.dataf.write(idata)
+	
+	def write_index(self):
+		idata = struct.pack(self.format, self.id, self.flags, self.data_size, self.disk_size, self.position)
+		self.index.write(idata)
 
 	def get_data(self):
 		idata = struct.pack(self.format, self.id, self.flags, self.data_size, self.disk_size, self.position)
 		return idata, self.data
 
-	def iterate(self, want_removed=False):
+	def iterate(self, want_removed=False, over_data=False):
 		while True:
-			self.read_index()
+			if over_data:
+				self.read_data_index()
+			else:
+				self.read_index()
+
 			if want_removed:
-				yield self.eid
+				yield self.id
 
 			if not self.removed():
-				yield self.eid
+				yield self.id
 
+	def sid(self, count=6):
+		ba = bytearray(self.id[0:count])
+		ret = ''
+		for i in range(count):
+			ret += '%02x' % ba[i]
+
+		return ret
