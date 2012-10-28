2ebe5f5793ccd799eb095e91cad21f8e0635839a
Alexey Ivanov <SaveTheRbtz@GMail.com>
2012-10-28 17:16:48 +0800

    binlog: make EBLOB_WARNX useful for routines other than binlog itself

--- library/binlog.h
+++ library/binlog.h
@@ -149,7 +149,7 @@ struct eblob_binlog_disk_record_hdr {
 
 /* Logging helpers */
 #define EBLOB_WARNX(log, severity, fmt, ...)	eblob_log(log, severity, \
-		"blob: binlog: %s: " fmt "\n", __func__ , ## __VA_ARGS__);
+		"blob: %s: " fmt "\n", __func__ , ## __VA_ARGS__);
 
 #define EBLOB_WARNC(log, severity, err, fmt, ...)	EBLOB_WARNX(log, severity, \
 		"%s (%ld); " fmt, strerror(err), (long int)err , ## __VA_ARGS__);
