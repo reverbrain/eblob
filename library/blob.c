5aff81247c9bfd7ac9adc26cc7fb887dc153461d
Alexey Ivanov <SaveTheRbtz@GMail.com>
2013-01-20 20:01:01 +0700

    blob: assert on append to sorted blob

--- library/blob.c
+++ library/blob.c
@@ -1161,6 +1161,7 @@ static int eblob_write_prepare_disk(struct eblob_backend *b, struct eblob_key *k
 		}
 	}
 
+	assert(ctl->sorted != 1);
 
 	wc->data_fd = ctl->data_fd;
 	wc->index_fd = ctl->index_fd;
