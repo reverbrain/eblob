13145734145b16937874ead50120e0a175470c5c
Alexey Ivanov <SaveTheRbtz@GMail.com>
2013-02-06 01:29:45 +0530

    ci: add eblob_cpp_test to autotests

--- tests/ci/travis.sh
+++ tests/ci/travis.sh
@@ -9,5 +9,8 @@ yes | debuild -e CC -e CXX --prepend-path="/usr/local/bin/" -uc -us
 # Install packages
 sudo -- dpkg -i ../*.deb
 
+# Run cpp bindings test
+$(find . -name eblob_cpp_test)
+
 # Run stress test
 $(find . -name eblob_stress) -m0 -f100 -D0 -I30000 -o20000 -i1000 -r 1000 -S10 -F87
