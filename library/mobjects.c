ab6f37efd824e73a753053933c870d0206d94e68
Alexey Ivanov <SaveTheRbtz@GMail.com>
2013-03-24 22:38:28 +0400

    mobjects: do not lock hash in common case

--- library/mobjects.c
+++ library/mobjects.c
@@ -925,18 +925,20 @@ int eblob_lookup_type(struct eblob_backend *b, struct eblob_key *key, int type, 
 	struct eblob_ram_control *rc = NULL;
 
 	/* If l2hash is enabled - look in it first */
-	pthread_mutex_lock(&b->hash->root_lock);
-	if (b->cfg.blob_flags & EBLOB_L2HASH && type <= b->l2hash_max) {
-		err = eblob_l2hash_lookup(b->l2hash[type], key, res);
-		if (err != 0 && err != -ENOENT) {
-			pthread_mutex_unlock(&b->hash->root_lock);
-			eblob_log(b->cfg.log, EBLOB_LOG_ERROR,
-					"blob: %s: %s: l2hash lookup failed: type: %d: %d.\n",
-					eblob_dump_id(key->id), __func__, type, err);
-			goto err_out_exit;
+	if (b->cfg.blob_flags & EBLOB_L2HASH) {
+		pthread_mutex_lock(&b->hash->root_lock);
+		if (type <= b->l2hash_max) {
+			err = eblob_l2hash_lookup(b->l2hash[type], key, res);
+			if (err != 0 && err != -ENOENT) {
+				pthread_mutex_unlock(&b->hash->root_lock);
+				eblob_log(b->cfg.log, EBLOB_LOG_ERROR,
+						"blob: %s: %s: l2hash lookup failed: type: %d: %d.\n",
+						eblob_dump_id(key->id), __func__, type, err);
+				goto err_out_exit;
+			}
 		}
+		pthread_mutex_unlock(&b->hash->root_lock);
 	}
-	pthread_mutex_unlock(&b->hash->root_lock);
 
 	if (err) {
 		err = eblob_hash_lookup_alloc(b->hash, key, (void **)&rc, (unsigned int *)&size);
