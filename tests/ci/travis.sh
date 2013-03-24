9939afcd8366ad03e366b6c68375c85a592342e9
Alexey Ivanov <SaveTheRbtz@GMail.com>
2013-03-25 00:57:27 +0400

    ci: added blocksize + nofooter tests

--- tests/ci/travis.sh
+++ tests/ci/travis.sh
@@ -17,3 +17,4 @@ $(find . -name eblob_cpp_test)
 
 # Run stress test
 $(find . -name eblob_stress) -m0 -f100 -D0 -I30000 -o20000 -i1000 -r 1000 -S10 -F87
+$(find . -name eblob_stress) -m0 -f10 -b 64 -D0 -I3000 -o2000 -i100 -r 100 -S100 -F14
