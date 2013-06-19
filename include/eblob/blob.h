41e512ca6eeb23d1a5203e120e486faf56ff29db
Alexey Ivanov <SaveTheRbtz@GMail.com>
2013-06-19 04:36:05 +0400

    misc: define offsetof only if it's not defined

--- include/eblob/blob.h
+++ include/eblob/blob.h
@@ -70,11 +70,12 @@ extern "C" {
 
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 
-#undef offsetof
+#ifndef offsetof
 #ifdef __compiler_offsetof
 #define offsetof(TYPE,MEMBER) __compiler_offsetof(TYPE,MEMBER)
 #else
 #define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+#endif
 #endif
 
 enum eblob_log_levels {
