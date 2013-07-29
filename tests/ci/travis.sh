dc095c280e856b3a25de7a4ad66ac2b45db9f89e
Alexey Ivanov <SaveTheRbtz@GMail.com>
2013-07-29 04:39:24 +0400

    ci: added overwrite-heavy test with many threads

--- tests/ci/travis.sh
+++ tests/ci/travis.sh
@@ -15,6 +15,9 @@ $(find . -name test.py)
 # Run cpp bindings test
 $(find . -name eblob_cpp_test)
 
-# Run stress test
+# Big and small stress tests
 $(find . -name eblob_stress) -m0 -f1000 -D0 -I300000 -o20000 -i1000 -l0 -r 1000 -S10 -F87
 $(find . -name eblob_stress) -m0 -f100 -D0 -I30000 -o2000 -i100 -l0 -r 100 -S100 -F14
+
+# Overwrite-heavy test with many bases and threads
+$(find . -name eblob_stress) -f1000 -D0 -I100000 -i64 -r 40 -S10 -F64 -T32 -l0 -o 0
