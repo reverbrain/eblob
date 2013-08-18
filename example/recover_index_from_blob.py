59b593623a3db97d00f6509d10bd299504d2ed56
Alexey Ivanov <SaveTheRbtz@GMail.com>
2013-08-19 02:02:51 +0400

    examples: recover: allow passing of data files via argv

--- example/recover_index_from_blob.py
+++ example/recover_index_from_blob.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 import eblob
 
 if __name__ == '__main__':
-	try:
-		b = eblob.blob('/tmp/data.0', index_mode='w+b')
+	print >>sys.stderr, "Started"
+	for base in sys.argv[1:]:
+		try:
+			b = eblob.blob(base, index_mode='w+b')
+			for eid in b.iterate(want_removed=False, over_data=True):
+				print "%s: flags: %x, position: %d, data_size: %d, disk_size: %d" % \
+					(b.sid(count=64), b.flags, b.position, b.data_size, b.disk_size)
+				b.write_index()
+		except NameError as e:
+			print "Completed:", e
 
-		for eid in b.iterate(want_removed=False, over_data=True):
-			print "%s: flags: %x, position: %d, data_size: %d, disk_size: %d" % \
-				(b.sid(count=64), b.flags, b.position, b.data_size, b.disk_size)
-			b.write_index()
-	except NameError as e:
-		print "Completed:", e
+	print >>sys.stderr, "Finished"
