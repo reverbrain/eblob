84e98fc953b23f5aa9cfdea7ba7f74545744ad31
Alexey Ivanov <SaveTheRbtz@GMail.com>
2013-08-19 02:00:08 +0400

    examples: recover: protect against inclusion

--- example/recover_index_from_blob.py
+++ example/recover_index_from_blob.py
@@ -3,12 +3,13 @@
 
 import eblob
 
-try:
-	b = eblob.blob('/tmp/data.0', index_mode='w+b')
+if __name__ == '__main__':
+	try:
+		b = eblob.blob('/tmp/data.0', index_mode='w+b')
 
-	for eid in b.iterate(want_removed=False, over_data=True):
-		print "%s: flags: %x, position: %d, data_size: %d, disk_size: %d" % \
-			(b.sid(count=64), b.flags, b.position, b.data_size, b.disk_size)
-		b.write_index()
-except NameError as e:
-	print "Completed:", e
+		for eid in b.iterate(want_removed=False, over_data=True):
+			print "%s: flags: %x, position: %d, data_size: %d, disk_size: %d" % \
+				(b.sid(count=64), b.flags, b.position, b.data_size, b.disk_size)
+			b.write_index()
+	except NameError as e:
+		print "Completed:", e
