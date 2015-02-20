2dc9b14d091e77afaea22c5cf07eb93fae0a2489
abudnik <budnik27@gmail.com>
2015-02-20 19:24:19 +0300

    added check_record to blob_to_index converter

--- example/blob_to_index.cpp
+++ example/blob_to_index.cpp
@@ -12,12 +12,42 @@ off_t get_file_size(const std::string &filename)
     return rc == 0 ? stat_buf.st_size : -1;
 }
 
+inline bool check_record(const struct eblob_disk_control &dc, uint64_t offset, size_t blob_length)
+{
+	static const uint64_t hdr_size = sizeof(struct eblob_disk_control);
+	if (dc.disk_size < offset + hdr_size) {
+		std::cerr << "malformed entry: disk_size is less than data_size + hdr_size: "
+			"offset: " << offset << '\n' <<
+			"key: " << eblob_dump_id(dc.key.id) << std::endl;
+
+		if (dc.disk_size == 0 && dc.data_size == 0) {
+			std::cerr << "... and it is zero-sized entry" << std::endl;
+		}
+		return false;
+	}
+
+	if (blob_length < offset + dc.disk_size) {
+		std::cerr << "malformed entry: offset + data_size is outside of blob: "
+			"offset: " << offset << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
 int main(int argc, char *argv[])
 {
 	if (argc < 2) {
 		std::cerr << "Usage: " << argv[0] << " /path/to/blob" << std::endl;
 		return 1;
 	}
+
+	off_t ret = get_file_size(argv[1]);
+	if (ret <= 0) {
+		std::cerr << "Blob file " << argv[1] << " is empty. Exiting..." << std::endl;
+		return 1;
+	}
+	size_t blob_length = static_cast<size_t>(ret);
 
 	std::string index_file = std::string(argv[1]) + ".index";
 	if (get_file_size(index_file) > 0) {
@@ -27,7 +57,7 @@ int main(int argc, char *argv[])
 
 	struct eblob_disk_control dc;
 	uint64_t total = 0, removed = 0;
-	std::streampos offset = 0;
+	uint64_t offset = 0;
 
 	try {
 		std::ifstream blob(argv[1], std::ifstream::in | std::ifstream::binary);
@@ -54,6 +84,9 @@ int main(int argc, char *argv[])
 			    throw std::runtime_error("Index read failed");
 			}
 
+			if (!check_record(dc, offset, blob_length))
+			    throw std::runtime_error("Found malformed entry");
+
 			++total;
 			if (dc.flags & BLOB_DISK_CTL_REMOVE)
 				++removed;
@@ -63,6 +96,7 @@ int main(int argc, char *argv[])
 		}
 	} catch (const std::exception &e) {
 		std::cerr << e.what() << std::endl;
+		std::cout << "Index extraction aborted" << std::endl;
 	}
 
 	std::cout << "Total records: " << total << std::endl;
